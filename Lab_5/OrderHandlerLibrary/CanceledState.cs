34752cba08e26c7dbf095932e9583bb097bc43cb
Dariia444 <76495350+Dariia444@users.noreply.github.com>
2022-06-27 21:54:28 +0200

    added lab_5 and diagram to lab_4

---
+++ Lab_5/OrderHandlerLibrary/CanceledState.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderHandlerLibrary
+{
+    internal class CanceledState : State
+    {
+        internal override void Handle()
+        {
+           
+        }
+
+        internal override void Cancel()
+        {
+
+        }
+    }
+}
